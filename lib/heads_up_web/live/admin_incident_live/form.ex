6063ed40ba213f1bc568eff78ffa3bf19e8a4e34
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-03-03 15:17:40 +0300

    create raffle

--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -1,5 +1,6 @@
 defmodule HeadsUpWeb.AdminIncidentLive.Form do
   use HeadsUpWeb, :live_view
+  alias HeadsUp.Admin
 
   def mount(_params, _session, socket) do
     socket =
@@ -15,7 +16,7 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
     <.header>
       <h1>{@page_title}</h1>
     </.header>
-    <.simple_form for={@form} id="incident-form" >
+    <.simple_form for={@form} id="incident-form" phx-submit="save">
     <.input field={@form[:name]} label="Name"/>
     <.input field={@form[:description]} label="Description" type="textarea"/>
     <.input field={@form[:priority]} label="Priority" type="number"/>
@@ -26,10 +27,17 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
       prompt="Choose Status"/>
       <.input field={@form[:image_path]} label="Image Path" />
       <:actions>
-        <.button type="submit">Save Incident</.button>
+        <.button phx-disable-with="Saving...">Save Incident</.button>
       </:actions>
     </.simple_form>
     <.back navigate={~p"/admin/incidents"}>Back</.back>
     """
+  end
+
+  def handle_event("save", %{"incident" => incident_params}, socket) do
+    _incident = Admin.create_incident(incident_params)
+    socket = push_navigate(socket, to: ~p"/admin/incidents")
+    {:noreply, socket}
+
   end
 end
