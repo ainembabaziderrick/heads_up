344dc6338776bc2705a1a8018825faaba2726d2b
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-03-05 11:08:05 +0300

    JS commands

--- lib/heads_up_web/live/admin_incident_live/index.ex
+++ lib/heads_up_web/live/admin_incident_live/index.ex
@@ -16,13 +16,26 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
   def render(assigns) do
     ~H"""
     <div class="admin-index">
-      <.header>
+      <.button phx-click={
+        JS.toggle(
+          to: "#joke",
+          in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+          out: {"ease-in-out duration-300", "opacity-100", "opacity-0"},
+          time: 300
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div class="joke hidden" id="joke">
+        Why shouldn't you trust trees?
+      </div>
+      <.header class="mt-6">
         <h1>{@page_title}</h1>
         <:actions>
           <.link class="button" navigate={~p"/admin/incidents/new"}>New Incident</.link>
         </:actions>
       </.header>
-      <.table id="incidents" rows={@streams.incidents}>
+      <.table id="incidents" rows={@streams.incidents} row_click={fn {_, incident} -> JS.navigate(~p"/incidents/#{incident}") end}>
         <:col :let={{_dom_id, incident}} label="Name">
           <.link navigate={~p"/incidents/#{incident}"}>
             {incident.name}
@@ -39,8 +52,8 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
             Edit
           </.link>
         </:action>
-        <:action :let={{_dom_id, incident}}>
-          <.link phx-click="delete" phx-value-id={incident.id} data-confirm="Are you sure to delete?">
+        <:action :let={{dom_id, incident}}>
+          <.link phx-click={delete_and_hide(dom_id, incident)} phx-value-id={incident.id} data-confirm="Are you sure to delete?">
             <.icon name="hero-trash" class="h-4 w-4" />
           </.link>
         </:action>
@@ -53,5 +66,12 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
     incident = Admin.get_incident!(id)
     {:ok, _} = Admin.delete_incident(incident)
     {:noreply, stream_delete(socket, :incidents, incident)}
+  end
+
+  def delete_and_hide(dom_id, incident) do
+    JS.push("delete",
+    value: %{id: incident.id}
+  )
+  |> JS.hide(to: "##{dom_id}", transition: "fade-out")
   end
 end
